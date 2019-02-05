66bb4ac7690bc81c26f85d029fc5d08df312fb30
Alvarez <aalvarez@tbccorp.com>
2019-02-05 16:20:58 -0500

    2

--- js/site.js
+++ js/site.js
@@ -1,13 +1,21 @@
 //mix-mosaic
 
-let boxTiles = document.getElementById('box-tiles');
+let boxTiles = document.getElementById('box-tiles'),
+tileImage = document.getElementById('tile-image');
+
 
 function tileBox(){
 
     //Create Tiles
     for ( let i = 0; i < 18; i++ ){
-        boxTiles.innerHTML += '<a href="#"><div' + + 'class="p-3 bd-highlight tile">' + + '</div></a>';
+        boxTiles.innerHTML += '<a href="#"><div id="tile-image" class="p-3 bd-highlight tile"></div></a>';
     };
+
+    function loadImage(){
+        tileImage = "img/";
+
+        
+    }
 }
 
 window.onload = tileBox();
